76064e0305821890c0a24a9318ba1ac1b36ec483
unknown <pritampaul9798@gmail.com>
2021-03-29 16:29:07 +0530

    added uc2 file - the player rolls the die

---
+++ uc2.java
@@ -0,0 +1,16 @@
+package snake;
+
+import java.util.*;
+
+public class uc2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake ladder game ");	
+		int pos=0;
+		System.out.println("initial position "+pos);
+		Random a = new Random();
+		int die = a.nextInt(6) + 1;
+		System.out.println("die rolled with a value of " +die);
+	}
+
+}
