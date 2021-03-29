3e7bd81dda1035dcb53c7fedb59ab7c704873d93
unknown <pritampaul9798@gmail.com>
2021-03-29 16:57:26 +0530

    added uc5 file - the end file

---
+++ uc5.java
@@ -0,0 +1,72 @@
+package snake;
+
+import java.util.*;
+
+public class uc5 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake ladder game ");
+		int p1 = 0;
+		int p2 = 0;
+		for (int i =0; i <=1; i++)
+		{
+		for (int pos=0; pos<=100;)
+		{
+			if (pos < 0 ) {
+				pos = 0;
+			}
+		System.out.println(" position "+pos);
+		Random a = new Random();
+		int die = a.nextInt(6) + 1;
+		if (i == 0) {
+		p1 = p1++;
+		}
+		else
+		{
+		p2 = p2++;
+		}
+		
+		System.out.println("die rolled with a value of " +die);
+		int checker = a.nextInt(3);
+		System.out.println(checker);
+		if (checker == 0) {
+			pos = pos;
+			System.out.println("the player remained in the same position " +pos);
+		}
+		else if (checker == 1) 
+		{
+			
+			if ((pos + die) >100) {
+				
+			}
+			
+			else {
+				pos = pos + die;
+				System.out.println("the player moved to " +pos);	
+			}
+			
+		
+		}
+		else 
+		{
+			pos = pos - die ;
+		System.out.println("got snaked " +pos);
+		}	
+		
+		if (pos == 100) {
+			break;
+		}
+		}
+	}
+		if (p2>p1)
+		{
+			System.out.println("player 1 is winner");
+		}
+		else
+		{
+			System.out.println("player 2");
+		}
+		
+	}	
+
+	}
