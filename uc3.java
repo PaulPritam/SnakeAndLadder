0205dc441d6bd04bc23eaf2b0235afb9d9a4b0c2
unknown <pritampaul9798@gmail.com>
2021-03-29 16:45:49 +0530

    added uc3 file to check the position

---
+++ uc3.java
@@ -0,0 +1,32 @@
+package snake;
+
+import java.util.*;
+
+public class uc3 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake ladder game ");	
+		int pos=0;
+		System.out.println("initial position "+pos);
+		Random a = new Random();
+		int die = a.nextInt(6) + 1;
+		System.out.println("die rolled with a value of " +die);
+		int checker = a.nextInt(3);
+		System.out.println(checker);
+		if (checker == 0) {
+			System.out.println("the player remained in the same position ");
+		}
+		else if (checker == 1) 
+		{
+			pos = die;
+			System.out.println("the player moved to " +pos);
+		}
+		else
+		{
+			pos = pos - die;
+			System.out.println(" the player moved to " +pos);
+		}
+			
+	}
+
+}
